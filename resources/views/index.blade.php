2b317535f5b5e3eec187125c0739185268904edf
mayu <mayu6v0@me.com>
2022-01-16 14:14:33 +0900

    mixをassetに戻す

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,8 +5,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>COACHTECH</title>
-  <link rel="stylesheet" href="{{mix('css/reset.css')}}" />
-  <link rel="stylesheet" href="{{mix('css/style.css')}}" />
+  <link rel="stylesheet" href="{{asset('css/reset.css')}}" />
+  <link rel="stylesheet" href="{{asset('css/style.css')}}" />
 </head>
 <body>
   <div class="container">
