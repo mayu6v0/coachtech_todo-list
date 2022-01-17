133ff81c7936337b29aaab5e73f8427160372363
mayu <mayu6v0@me.com>
2022-01-17 18:58:56 +0900

    secure_assetに@if追記

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,8 +5,14 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>COACHTECH</title>
+  @if(app('env')=='local')
   <link rel="stylesheet" href="{{asset('css/reset.css')}}" />
   <link rel="stylesheet" href="{{asset('css/style.css')}}" />
+  @endif
+  @if(app('env')=='production')
+  <link rel="stylesheet" href="{{secure_asset('css/reset.css')}}" />
+  <link rel="stylesheet" href="{{secure_asset('css/style.css')}}" />
+  @endif
 </head>
 <body>
   <div class="container">
